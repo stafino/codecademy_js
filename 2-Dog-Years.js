82fd3daa1c8813a679c0243e71c10935b6826211
Oliver Alan Stafurik <2444536S@student.gla.ac.uk>
2022-08-30 15:16:00 +0100

    Added Chapter 4 - Race Day + Added Chapter Description into comments

--- 2-Dog-Years.js
+++ 2-Dog-Years.js
@@ -1,3 +1,16 @@
+/*
+Dog Years
+Dogs mature at a faster rate than human beings. We often say a dog’s age can be calculated in “dog years” to account for their growth compared to a human of the same age. In some ways we could say, time moves quickly for dogs — 8 years in a human’s life equates to 45 years in a dog’s life. How old would you be if you were a dog?
+
+Here’s how you convert your age from “human years” to “dog years”:
+
+The first two years of a dog’s life count as 10.5 dog years each.
+Each year following equates to 4 dog years.
+Before you start doing the math in your head, let a computer take care of it! With your knowledge of math operators and variables, use JavaScript to convert your human age into dog years.
+
+If you get stuck during this project or would like to see an experienced developer work through it, click “Get Unstuck“ to see a project walkthrough video.
+*/
+
 let myName = 'oLIveR'.toLowerCase(); // my name
 let myAge = 22; // My Age
 let earlyYears = 2; // Early Dog Years are counted differently than it's later years.
--- 3-Eight-Ball.js
+++ 3-Eight-Ball.js
@@ -1,3 +1,14 @@
+/*
+Magic Eight Ball
+You’ve learned a powerful tool in JavaScript: control flow! It’s so powerful, in fact, that it can be used to tell someone’s fortune.
+
+In this project we will build a Magic Eight Ball using control flow in JavaScript.
+
+The user will be able to input a question, then our program will output a random fortune.
+
+If you get stuck during this project or would like to see an experienced developer work through it, click “Get Unstuck“ to see a project walkthrough video.
+*/
+
 // Declaring variables
 let userName = 'Jonas';
 let randomNumber = Math.floor(Math.random() * 8);

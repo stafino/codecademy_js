193ea0facd2b4d61e6d2ad6d73d4ad56282be356
Oliver Alan Stafurik <oliverstaf1@gmail.com>
2022-09-11 22:47:22 +0200

    Exercise 13 - Added class Media

---
+++ 13-Build-A-Library.js
@@ -0,0 +1,64 @@
+/*
+Build a Library
+Congratulations, you’ve become head librarian at your local Books-‘N-Stuff, which is in dire need of your help. They’re still using index cards to organize their content! Yikes.
+
+But no matter, you know some JavaScript, so let’s get to work modernizing your new digs.
+
+Books-‘N-Stuff carries three different types of media: books, CDs, and movies. In this project you will create a parent class named Media with three subclasses: Book, Movie, and CD. These three subclasses have the following properties and methods:
+
+Book
+
+Properties: author (string), title (string), pages (number), isCheckedOut (boolean, initially false), and ratings (array, initially empty).
+Getters: all properties have a getter
+Methods: .getAverageRating(), .toggleCheckOutStatus(), and .addRating()
+Movie
+
+Properties: director (string), title (string), runTime (number), isCheckedOut (boolean, initially false), and ratings (array, initially empty)
+Getters: all properties have a getter
+Methods: .getAverageRating(), .toggleCheckOutStatus(), and .addRating()
+CD
+
+Properties: artist (string), title (string), isCheckedOut (boolean, initially false), and ratings (array, initially empty), songs (array of strings)
+Getters: all properties have a getter
+Methods: .getAverageRating(), .toggleCheckOutStatus(), and .addRating()
+If you’re looking for a challenge, try to create the four classes without using the steps below.
+*/
+
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get isCheckedOut()  {
+    return this._isCheckedOut;
+  }
+
+  get ratings() {
+    return this._ratings;
+  }
+
+  set isCheckedOut(isCheckedOut)  {
+    this._isCheckedOut = isCheckedOut;
+  }
+
+  getAverageRating()  {
+    let resultSum = this._ratings.reduce((currentVal, rating) => currentVal + rating, 0);
+    return resultSum / this._ratings.length;
+  }
+
+  toggleCheckOutStatus()  {
+    return this._isCheckedOut = !this._isCheckedOut;
+  }
+
+  addRating(rating) {
+    this._ratings.push(rating);
+  }
+}
+
+
